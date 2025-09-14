1127575da7a1629a1524142028ac56c09341b69c
najmulHasanChowdhury <hnajmul39@gmail.com>
2025-09-15 01:08:33 +0600

    Hero Section Particles Added

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -3,10 +3,13 @@
 import Image from 'next/image'
 import React from 'react'
 import Typewriter from 'typewriter-effect';
+import { ImArrowRight } from "react-icons/im";
+import ParticlesHero from './ParticleBackground';
 
 const Hero = () => {
   return (
     <div className='relative h-screen flex items-center justify-center text-white overflow-hidden flex-col'>
+        <ParticlesHero />
         <div className='relative z-10 flex flex-col items-center'>
           <Image src="/images/s1.jpg" alt='heroImage' width={180} height={180} className='rounded-full border-8 border-[#0c0c48aa]' />
           <h1 className='text-2xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 text-center font-bold tracking-wide'>Developing web products, apps & <br/>
@@ -33,6 +36,11 @@ const Hero = () => {
             />
             </span>
           </h2>
+          <button className='mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300
+          cursor-pointer rounded-full text-lg font-medium'>
+            <span>Explore my work</span>
+            <ImArrowRight className='w-4 h-4 ml-2 inline-block' />
+          </button>
         </div>
     </div>
   )
