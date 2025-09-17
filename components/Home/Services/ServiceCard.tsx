c283d1d75f4c7704cd08251dabba4b9c63825b8a
najmulHasanChowdhury <hnajmul39@gmail.com>
2025-09-17 20:20:33 +0600

    Services Section complete

--- components/Home/Services/ServiceCard.tsx
+++ components/Home/Services/ServiceCard.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
-const ServiceCard = () => {
-  return <div>ServiceCard</div>;
+type Props = {
+  icon: string;
+  name: string;
+  description: string;
+};
+
+const ServiceCard = ({ icon, name, description }: Props) => {
+  return (
+    <div>
+      <Image src={icon} alt="img" width={60} height={60} />
+      <h1 className="mt-3 text-xl md:text-2xl font-bold text-green-200">
+        {name}
+      </h1>
+      <p className="mt-2 text-gray-200">{description}</p>
+    </div>
+  );
 };
 
 export default ServiceCard;
--- components/Home/Services/Services.tsx
+++ components/Home/Services/Services.tsx
@@ -10,7 +10,32 @@ const Services = () => {
       </h1>
       <div className="w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 mt-20 items-center">
         <div>
-          <ServiceCard />
+          <ServiceCard
+            icon="/images/web-dev1.png"
+            name="Full-Stack Web Development"
+            description="Developing Websites that are interacticve, efficient and user-friendly."
+          />
+        </div>
+        <div>
+          <ServiceCard
+            icon="/images/app-dev1.png"
+            name="App Development"
+            description="Developing Apps that are Cross-platform and user-friendly."
+          />
+        </div>
+        <div>
+          <ServiceCard
+            icon="/images/dev-Ops1.png"
+            name="Dev-Ops Engineering"
+            description="Responsible for all aspects of the software development lifecycle, including communicating critical information to the business and customers."
+          />
+        </div>
+        <div>
+          <ServiceCard
+            icon="/images/web-an1.png"
+            name="Web analytics & conversion tracking"
+            description="Collecting, measuring, analyzing, and reporting data from websites and digital platforms to understand user behavior and optimize online performance."
+          />
         </div>
       </div>
     </div>
