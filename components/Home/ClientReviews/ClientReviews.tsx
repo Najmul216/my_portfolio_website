23d72dc05497e9afdbf0381977ae4cf7ad13f1aa
najmulHasanChowdhury <hnajmul39@gmail.com>
2025-09-23 23:45:03 +0600

    Review Section Add

---
+++ components/Home/ClientReviews/ClientReviews.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ClientReviews = () => {
+  return (
+    <div className="pt-16 pb-16">
+      <div className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white">
+        Kind reviews from satisfied <br />
+        <span className="text-cyan-300">Clients</span>
+      </div>
+    </div>
+  );
+};
+
+export default ClientReviews;
