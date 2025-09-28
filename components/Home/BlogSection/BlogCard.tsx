6e084ce3e1b4ef7f03675a0f63ab4eaeae726e90
najmulHasanChowdhury <hnajmul39@gmail.com>
2025-09-28 19:56:02 +0600

    Blog Section Completed

--- components/Home/BlogSection/BlogCard.tsx
+++ components/Home/BlogSection/BlogCard.tsx
@@ -19,6 +19,20 @@ const BlogCard = ({ image, title }: Props) => {
       <p className="mt-4 text-gray-400 font-medium text-base sm:text-lg">
         28 September 2025
       </p>
+      <h1 className="mt-5 text-lg sm:text-xl font-bold text-white hover:underline hover:text-cyan-300 cursor-pointer transition-all duration-300">
+        {title}
+      </h1>
+      <div className="mt-4 flex gap-2 items-center">
+        <p className="px-4 py-1.5 bg-blue-950 text-white text-sm sm:text-base font-bold rounded-full">
+          Next.js
+        </p>
+        <p className="px-4 py-1.5 bg-blue-950 text-white text-sm sm:text-base font-bold rounded-full">
+          React.js
+        </p>
+        <p className="px-4 py-1.5 bg-blue-950 text-white text-sm sm:text-base font-bold rounded-full">
+          Tailwind CSS
+        </p>
+      </div>
     </div>
   );
 };
