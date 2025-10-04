33afd3e1fa8a52fc27e8d5fecb558ab5289346ab
najmulHasanChowdhury <hnajmul39@gmail.com>
2025-10-04 12:24:13 -0500

    Install & Implement AOS Animation

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import Hero from "./Hero/Hero";
 import Services from "./Services/Services";
 import Resume from "./Resume/Resume";
@@ -7,8 +9,23 @@ import Skills from "./Skills/Skills";
 import ClientReviews from "./ClientReviews/ClientReviews";
 import BlogSection from "./BlogSection/BlogSection";
 import Contact from "./Contact/Contact";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 
 const Home = () => {
+  useEffect(() => {
+    const initAOS = async () => {
+      await import("aos");
+      AOS.init({
+        duration: 1000,
+        easing: "ease",
+        once: true,
+        anchorPlacement: "top-bottom",
+      });
+    };
+    initAOS();
+  }, []);
+
   return (
     <div className="overflow-hidden text-white">
       <Hero />
